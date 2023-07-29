9f60e4742d3ed9a109be8baad2c2aa30837d930e
Pablo X <jprodsanz@gmail.com>
2023-07-29 04:58:57 -0500

    opening/closing ham menu

--- position-09-assignment-problem/shared.js
+++ position-09-assignment-problem/shared.js
@@ -1,6 +1,9 @@
 const backdrop = document.querySelector('.backdrop');
+const modalNoButton = document.querySelector('.modal__action--negative')
 const selectPlanButtons = document.querySelectorAll('.plan button');
 const modal = document.querySelector('.modal')
+const toggleButton = document.querySelector('.toggle-button')
+const mobileNav = document.querySelector('.mobile-nav')
 
 // backdrop.style.display = 'block';
 // console.dir(selectPlanButtons);
@@ -9,5 +12,21 @@ for(let i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons [i].addEventListener('click', function () {
         modal.style.display = 'block';
         backdrop.style.display = 'block';
-    })
+    });
 }
+backdrop.addEventListener('click', function (){
+    mobileNav.style.display = 'none';
+    closeModal()
+});
+
+modalNoButton.addEventListener('click', closeModal);
+
+function closeModal(){
+    backdrop.style.display = 'none';
+    modal.style.display ='none';
+}
+toggleButton.addEventListener('click', function() {
+    mobileNav.style.display ='block';
+    backdrop.style.display = 'block';
+    }
+)
