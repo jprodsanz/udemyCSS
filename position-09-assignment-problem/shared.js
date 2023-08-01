becc9b1b3cf019a6cd3e3022f156379edd847519
Pablo X <jprodsanz@gmail.com>
2023-08-01 00:18:42 -0500

    cleaning up code

--- position-09-assignment-problem/shared.js
+++ position-09-assignment-problem/shared.js
@@ -10,23 +10,34 @@ const mobileNav = document.querySelector('.mobile-nav')
 
 for(let i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons [i].addEventListener('click', function () {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        // modal.className = 'open'; // this will overwrite the complete
+        modal.classList.add('open')
+        backdrop.classList.add('open')
     });
 }
 backdrop.addEventListener('click', function (){
-    mobileNav.style.display = 'none';
+    // mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
     closeModal()
 });
 
-modalNoButton.addEventListener('click', closeModal);
+if (modalNoButton) {
+    modalNoButton.addEventListener('click', closeModal);
+}
+
 
 function closeModal(){
-    backdrop.style.display = 'none';
-    modal.style.display ='none';
+    // backdrop.style.display = 'none';
+    // modal.style.display ='none';
+    modal.classList.remove('open');
+    backdrop.classList.remove('open');
 }
 toggleButton.addEventListener('click', function() {
-    mobileNav.style.display ='block';
-    backdrop.style.display = 'block';
+    // mobileNav.style.display ='block';
+    // backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open')
     }
 )
